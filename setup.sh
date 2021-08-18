77c766ab32794c4bfe6743db3d2e01074084e4c6
Chase Adams <chase@chases-mbp.lan>
2021-08-17 20:58:41 -0700

    feat: new install

--- setup.sh
+++ setup.sh
@@ -12,9 +12,10 @@ brew cleanup
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+git config --global core.editor "nano"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 chsh -s /bin/zsh
@@ -26,8 +27,17 @@ asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
 
+asdf install elixir latest
+asdf install erlang latest
+asdf install java latest
+asdf install nodejs latest
+asdf install ruby latest
+asdf install rust latest
+
 asdf global java latest
 asdf global nodejs latest
 asdf global ruby latest
 
 npm install --global yarn
+
+gh auth login
